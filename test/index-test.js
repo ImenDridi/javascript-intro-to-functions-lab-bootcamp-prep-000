19b9c5adc6029dac5dfae4eea58059d4392a7756
dridi.imen2014 Dridi <dridi.imen2014@gmail.com>
2018-09-18 10:37:31 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,13 +15,8 @@ describe('whisper(string)', function() {
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
+    
+    console.log(string.toUpperCase())
   })
 })
 
